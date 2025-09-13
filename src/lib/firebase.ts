0fe9c4afa9ad90d0e02fa4a27a62a18fa825093c
Ankit Sharma <ankit.sharma@zeeve.io>
2025-09-13 06:16:00 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'studio-2694253795-a36d9',
-  appId: '1:1049150617946:web:ab13f5a9add75c78645b63',
-  storageBucket: 'studio-2694253795-a36d9.firebasestorage.app',
-  apiKey: 'AIzaSyDzmuUlsa27J1wRBULFPF4cQ7DjydquMD0',
-  authDomain: 'studio-2694253795-a36d9.firebaseapp.com',
-  messagingSenderId: '1049150617946',
+  "projectId": "studio-2694253795-a36d9",
+  "appId": "1:1049150617946:web:ab13f5a9add75c78645b63",
+  "storageBucket": "studio-2694253795-a36d9.firebasestorage.app",
+  "apiKey": "AIzaSyDzmuUlsa27J1wRBULFPF4cQ7DjydquMD0",
+  "authDomain": "studio-2694253795-a36d9.firebaseapp.com",
+  "messagingSenderId": "1049150617946"
 };
 
 // Initialize Firebase
